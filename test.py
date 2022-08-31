66953b3305de172f92f22c9090e0f8db2369bd5b
raylyao <raylyao@tencent.com>
2022-08-31 11:30:41 +0800

    train by sb3

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
+import os
+from os.path import dirname, abspath, join
 import gym
 import ur10_mujoco_gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.logger import configure
 
 if __name__ == '__main__':
-    env = gym.make('UR10Reach-v0')
-    print(env.observation_space)
-    print(env.action_space)
-    print(env.reset())
-    action = env.action_space.sample()
-    for i in range(1000):
-        action = env.action_space.sample()
-        obs, _, _, info = env.step(action)
-        env.render()
+    logdir = dirname(abspath(__file__)) + '/tmp/UR10'
+    os.makedirs(logdir, exist_ok=True)
+    env = gym.make('UR10Reach-v0', reward_type='dense')
+    # env = gym.make('FetchReach-v1', reward_type='dense')
+    # set up logger
+    new_logger = configure(logdir, ["stdout", "log", "tensorboard", "csv", "json"])
+    env = Monitor(env, logdir, info_keywords=("is_success",))
+    model = PPO('MultiInputPolicy', env, verbose=1)
+    model.set_logger(new_logger)
+    model.learn(total_timesteps=50_0000)
--- ur10_mujoco_gym/__init__.py
+++ ur10_mujoco_gym/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 register(
     id='UR10Reach-v0',
     entry_point='ur10_mujoco_gym.envs:URReachEnv',
-    # max_episode_steps=50,
+    max_episode_steps=50,
 )
